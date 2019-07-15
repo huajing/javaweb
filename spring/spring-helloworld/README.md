26f073904efa83286a5b611d3eb10018efcd37e0
chenck <zekuan2301155065@126.com>
2019-07-15 23:42:51 +0800

    add spring dir

---
+++ spring/spring-helloworld/README.md
@@ -0,0 +1,7 @@
+## 最简单获取bean
+---
+依赖：spring-context-suppert  
+        --spring-beans  
+        --spring-context    
+        --spring-core   
+    
---
+++ spring/spring-helloworld/src/main/java/c/c/k/HelloWorld.java
@@ -0,0 +1,14 @@
+package c.c.k;
+
+public class HelloWorld {
+
+    private String message;
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+}
