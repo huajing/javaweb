8f52aef1b020f23c03094d79d6fae6b0c1918839
chenck <chris@heiyan.com>
2019-07-20 10:47:19 +0800

    add config

--- spring-cloud/spring-config/spring-config-server/src/main/java/c/c/k/ConfigServerApplication.java
+++ spring-cloud/spring-config/spring-config-server/src/main/java/c/c/k/ConfigServerApplication.java
@@ -4,6 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.config.server.EnableConfigServer;
 
+//开启配置服务器
 @EnableConfigServer
 @SpringBootApplication
 public class ConfigServerApplication {
