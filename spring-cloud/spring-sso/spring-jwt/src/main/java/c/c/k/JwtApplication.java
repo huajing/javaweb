262cbd66bd53edf934d9b7dcb60c38c093abc836
chenck <zekuan2301155065@126.com>
2019-08-28 23:40:56 +0800

    sso

--- spring-cloud/spring-sso/spring-jwt/src/main/java/c/c/k/JwtApplication.java
+++ spring-cloud/spring-sso/spring-jwt/src/main/java/c/c/k/JwtApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class ShareSessionApplication {
+public class JwtApplication {
     public static void main(String[] args) {
-        SpringApplication.run(ShareSessionApplication.class, args);
+        SpringApplication.run(JwtApplication.class, args);
     }
 }
