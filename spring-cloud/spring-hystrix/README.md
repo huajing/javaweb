237357ba01973cfe8af154458ee2560dba1b0d0d
chenck <zekuan2301155065@126.com>
2019-09-30 01:25:26 +0800

    测试histrix

---
+++ spring-cloud/spring-hystrix/README.md
@@ -0,0 +1,4 @@
+## 说明
+https://www.cnblogs.com/happyflyingpig/p/8079308.html
+
+HystrixCommand，直接加到接口上，如HelloService
---
+++ spring-cloud/spring-hystrix/src/main/java/c/c/k/ctn/MyHystrixCommand.java
@@ -0,0 +1,23 @@
+package c.c.k.ctn;
+
+import com.netflix.hystrix.HystrixCommand;
+import com.netflix.hystrix.HystrixCommandGroupKey;
+
+public class MyHystrixCommand extends HystrixCommand<String> {
+    static int idx;
+    public MyHystrixCommand(){
+        super(HystrixCommandGroupKey.Factory.asKey("name1"));
+    }
+    @Override
+    protected String run() throws Exception {
+        idx++;
+        if(idx>10)
+            throw new RuntimeException();
+        return "succ";
+    }
+
+    @Override
+    protected String getFallback() {
+        return "tail";
+    }
+}
