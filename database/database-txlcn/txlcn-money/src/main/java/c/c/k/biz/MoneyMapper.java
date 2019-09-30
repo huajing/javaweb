816f049c2e50474ca422532beed6f6aff2ca097d
chenck <chris@heiyan.com>
2019-09-30 09:50:39 +0800

    删除txlcn,增加引用

--- database/database-txlcn/txlcn-money/src/main/java/c/c/k/biz/MoneyMapper.java
+++
@@ -1,8 +0,0 @@
-package c.c.k.biz;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface MoneyMapper extends BaseMapper<Money> {
-}
--- database/database-txlcn/txlcn-order/src/main/java/c/c/k/biz/OrderMapper.java
+++
@@ -1,9 +0,0 @@
-package c.c.k.biz;
-
-import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import org.apache.ibatis.annotations.Mapper;
-
-@Mapper
-public interface OrderMapper extends BaseMapper<Order> {
-
-}
