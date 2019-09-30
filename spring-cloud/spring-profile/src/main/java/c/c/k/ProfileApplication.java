f866d37e23f77fc42f07cb70a7adfb06d7a34b17
chenck <chris@heiyan.com>
2019-09-30 09:44:24 +0800

    添加 spring 动态 profile

---
+++ spring-cloud/spring-profile/src/main/java/c/c/k/ProfileApplication.java
@@ -0,0 +1,17 @@
+package c.c.k;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Title c.c.k
+ * @Copyright: Copyright 2019
+ * @Description: java <br/>
+ * @Created on 2019/9/19 chenck
+ */
+@SpringBootApplication
+public class ProfileApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ProfileApplication.class, args);
+    }
+}
