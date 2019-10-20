ec8b4194b83b1133b61540750d648b7ae2dd85d8
chenck <zekuan2301155065@126.com>
2019-10-20 09:56:08 +0800

    zuul fallback

--- spring-cloud/spring-gate/spring-zuul/src/main/java/c/c/k/MyFallback.java
+++ spring-cloud/spring-gate/spring-zuul/src/main/java/c/c/k/MyFallback.java
@@ -2,8 +2,13 @@ package c.c.k;
 
 import com.netflix.zuul.ZuulFilter;
 import org.springframework.cloud.netflix.zuul.filters.route.FallbackProvider;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.client.ClientHttpResponse;
 import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * zuul的全局回退，可实现按服务的histrix
@@ -17,6 +22,36 @@ public class MyFallback implements FallbackProvider {
 
     @Override
     public ClientHttpResponse fallbackResponse(String route, Throwable cause) {
-        return null;
+        return new ClientHttpResponse() {
+            @Override
+            public HttpStatus getStatusCode() throws IOException {
+                return null;
+            }
+
+            @Override
+            public int getRawStatusCode() throws IOException {
+                return 0;
+            }
+
+            @Override
+            public String getStatusText() throws IOException {
+                return null;
+            }
+
+            @Override
+            public void close() {
+
+            }
+
+            @Override
+            public InputStream getBody() throws IOException {
+                return null;
+            }
+
+            @Override
+            public HttpHeaders getHeaders() {
+                return null;
+            }
+        };
     }
 }
