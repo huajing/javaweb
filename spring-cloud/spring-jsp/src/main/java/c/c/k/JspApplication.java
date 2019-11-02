90d5185b666476b3d14ae05bb20b5d87900132da
chenck <zekuan2301155065@126.com>
2019-11-02 09:27:28 +0800

    add jsp

--- spring-cloud/spring-jsp/src/main/java/c/c/k/JspApplication.java
+++ spring-cloud/spring-jsp/src/main/java/c/c/k/JspApplication.java
@@ -1,10 +1,21 @@
 package c.c.k;
 
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
-
+@SpringBootApplication
+@Controller
 public class JspApplication {
     public static void main(String[] args) {
+        SpringApplication.run(JspApplication.class, args);
+    }
 
+    @GetMapping("/login")
+    public String login(){
+        System.out.println("login page.......");
+        return "login";
     }
 }
