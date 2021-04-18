cbf1e4be5a68ef7a3c8fe84705905f7762fb3b96
chenck <chris@heiyan.com>
2021-04-18 23:15:56 +0800

    add nacos test

---
+++ spring-cloud/spring-nacos/nacos-service1/src/main/java/cck/Service1Application.java
@@ -0,0 +1,17 @@
+package cck;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Title cck
+ * @Copyright: Copyright 2021
+ * @Description: java <br/>
+ * @Created on 2021/4/18 chenck
+ */
+@SpringBootApplication
+public class Service1Application {
+    public static void main(String[] args) {
+        SpringApplication.run(Service1Application.class, args);
+    }
+}
